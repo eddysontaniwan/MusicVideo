1bb02526916481389830235d9475a704dda74346
Eddyson <eddyson@nomad.id>
2016-06-01 13:13:55 +0700

    Completed Part 15

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -26,6 +26,39 @@ class MusicVideoTableViewCell: UITableViewCell {
         
         musicTitle.text = video?.vName
         rank.text = ("\(video!.vRank)")
-        musicImage.image = UIImage(named: "no-image")
+//        musicImage.image = UIImage(named: "no-image")
+        
+        if video!.vImageData != nil {
+            print("Get data from array...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        } else {
+            getVideoImage(video!, imageView: musicImage)
+        }
     }
+    
+    func getVideoImage(video: Videos, imageView : UIImageView) {
+        // Background thread
+        // DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high priority, i.e. the queue will be scheduled for execution before any default priority or low priority queue
+        //
+        // DISPATCH_QUEUE_PRIORITY_DEFAULT Items dispatched to the queue will run at the default priority, i.e. the queue will be scheduled for execution after all high priority queues have been scheduled, but before any low priority queues have been scheduled.
+        // DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at liw priority, i.e. the queue will be scheduled for execution after all default priority and high priority queues have been scheduled.
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image : UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+            
+        }
+    }
+    
 }
