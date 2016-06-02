46ce827b73e85953a4247229d7359f27ada4fc93
Eddyson <eddyson@nomad.id>
2016-06-02 23:00:33 +0700

    Completed Part 22

---
+++ MusicVideo/SettingTVC.swift
@@ -0,0 +1,52 @@
+//
+//  SettingTVC.swift
+//  MusicVideo
+//
+//  Created by Eddyson Tan on 6/2/16.
+//  Copyright © 2016 Eddyson Tan. All rights reserved.
+//
+
+import UIKit
+
+class SettingTVC: UITableViewController {
+
+    @IBOutlet weak var aboutDisplay: UILabel!
+    
+    @IBOutlet weak var feedbackDisplay: UILabel!
+    
+    @IBOutlet weak var securityDisplay: UILabel!
+    
+    @IBOutlet weak var touchID: UISwitch!
+    
+    @IBOutlet weak var bestImageDisplay: UILabel!
+    
+    @IBOutlet weak var APICnt: UILabel!
+    
+    @IBOutlet weak var sliderCnt: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SettingTVC.preferedFontChanged), name: UIContentSizeCategoryDidChangeNotification, object: nil)
+        
+        tableView.alwaysBounceVertical = false
+    }
+
+    func preferedFontChanged() {
+        
+        aboutDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        feedbackDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        securityDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        bestImageDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        APICnt.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+    
+}
